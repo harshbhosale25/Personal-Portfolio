d124f68c63d20c06f0caf3662daaad8c9c4abe6b
harshbhosale25 <harshadbhosale25202@gmail.com>
2025-06-24 11:57:34 +0530

    Update work.jsx

--- src/components/Body/Work/Work.jsx
+++ src/components/Body/Work/Work.jsx
@@ -61,25 +61,43 @@ const Work = () => {
             <div className="work-card bg-black">
               <h2 className="work-title">Project 1</h2>
               <p className="work-description">
-                The project is a React Native application...
+                The project is a React Native application designed to offer a
+                seamless user experience for e-commerce functionality. It includes
+                key features such as product browsing, category filtering, cart
+                management, and checkout. The app utilizes various reusable
+                components and state management techniques to ensure optimal
+                performance and scalability
               </p>
             </div>
             <div className="work-card bg-black">
               <h2 className="work-title">Project 2</h2>
               <p className="work-description">
-                The project is a backend application...
+              The project is a backend application built using Express and
+              Node.js, designed to provide RESTful APIs for a web and mobile
+              application. It serves as the backbone of the system, managing data
+              flow between the frontend and a database. The application handles
+              essential functionality like user authentication, product
+              management, cart operations, and order processing.
               </p>
             </div>
             <div className="work-card bg-black">
               <h2 className="work-title">Project 3</h2>
               <p className="work-description">
-                The project is a data-driven application...
+                The project is a React Native mobile application designed to provide
+                a user-friendly interface and cross-platform compatibility. It
+                focuses on delivering a seamless experience for functionalities such
+                as browsing, user interaction, data display, and integration with
+                backend APIs
               </p>
             </div>
             <div className="work-card bg-black">
               <h2 className="work-title">Project 4</h2>
               <p className="work-description">
-                The project is a React Native mobile application...
+              The project is a React Native mobile application designed to provide
+              a user-friendly interface and cross-platform compatibility. It
+              focuses on delivering a seamless experience for functionalities such
+              as browsing, user interaction, data display, and integration with
+              backend APIs
               </p>
             </div>
           </Carousel>
